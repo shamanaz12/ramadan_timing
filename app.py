cd38d5b8d18670e2bfd9ce565ad1de0db14cc096
shamanaz12 <160182090+shamanaz12@users.noreply.github.com>
2025-03-02 21:27:32 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Unit Converter", layout="wide")
+
+st.title("Unit Conversion App")
+
+st.write("Select a page from the sidebar to start converting units!")
